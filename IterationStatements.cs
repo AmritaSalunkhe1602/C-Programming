b8589368ef53585f9181feaa35511795e880a324
AmritaSalunkhe1602 <103725625+AmritaSalunkhe1602@users.noreply.github.com>
2022-07-26 09:20:45 +0530

    Control Statements of Iteration type in C#.

---
+++ IterationStatements.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlSatements1
+{
+    class IterationStatements
+    {
+        static void Main(string[] args)
+        {
+            /*For loop
+            for(int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+            */
+
+            /*while loop Entry Controlled Loop
+            int i = 10;
+            while (i>=1)
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+            Console.ReadLine();
+            */
+
+            /*do while loop Exit Controlled loop*/
+            int i = 1;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i <= 10);
+               
+            
+            Console.ReadLine();
+        }
+
+    }
+}
