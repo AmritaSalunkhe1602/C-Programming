998aa171f3916cbcf9281e6349bab33374e13871
AmritaSalunkhe1602 <103725625+AmritaSalunkhe1602@users.noreply.github.com>
2022-07-26 09:21:18 +0530

    Jump Statements in C#.

---
+++ JumpStatements.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlStatements2
+{
+    class JumpStatements
+    {
+        static void Main(string[] args)
+        {
+            /*Break Statement
+            for(int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+
+                if (i == 5)
+                {
+                    break;
+                }
+            }
+            Console.ReadLine();
+            */
+
+            /*Continue Statements*/
+             for(int i = 0; i <= 10; i++)
+            {
+                if (i == 7)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
