e0a48c57a641758c1afc1719aaeae1cdccbd865a
AmritaSalunkhe1602 <103725625+AmritaSalunkhe1602@users.noreply.github.com>
2022-07-26 09:19:47 +0530

    Control Statements of Conditional types in C#.

---
+++ ConditionalStatements.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlStatements
+{
+    class ConditionalStatements
+    {
+        static void Main(string[] args)
+        {
+            /*if statement
+            int number = 39;
+            if (number == 29)
+            {
+                Console.WriteLine("Number is 39");
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+            */
+
+            /*if-else statement
+            int number1 = 10;
+            if (number1 == 11)
+            {
+                Console.WriteLine("Number is same");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Number is not same");
+                Console.ReadLine();
+            }
+            */
+
+            /*if-else-if ladder statement
+            int number2 = 400;
+            if (number2 == 10)
+            {
+                Console.WriteLine("Number is 10");
+            }
+            else if (number2 == 20)
+            {
+                Console.WriteLine("Number is 20");
+            }
+            else if(number2 == 30)
+            {
+                Console.WriteLine("Number is 30");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Number");
+            }
+            Console.ReadLine();
+            */
+
+            /*nested if statement
+            int number3 = 10;
+            int number4 = 30;
+            if (number3 == 10)
+            {
+                if (number4 == 20)
+                {
+                    Console.WriteLine("Numbers are 10 and 20");
+
+                }
+                else
+                {
+                    Console.WriteLine("invalid numbers");
+                }
+            }
+            
+            Console.ReadLine();
+            */
+
+            /*Switch Statement
+            int number5 = 40;
+            switch (number5)
+            {
+                case 10:
+                    Console.WriteLine("Number is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("Number is 20");
+                    break;
+                case 30:
+                    Console.WriteLine("Number is 30");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Number");
+                    break;
+            }
+            Console.ReadLine();
+            */
+        }
+    }
+}
